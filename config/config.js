0811264a7c2a79366b4c22995064053d47d50836
Ajay-Das-k <ajtechhub01@gmail.com>
2024-02-29 12:42:00 +0530

    first commit

---
+++ config/config.js
@@ -0,0 +1,10 @@
+const sessionSecret = "mysitesessionsecret";
+
+const emailUser='robanprojectcsegrp6@gmail.com'
+const emailPassword='acwe ipon qlgt aogy'
+
+module.exports ={
+   sessionSecret,
+   emailUser,
+   emailPassword
+}
